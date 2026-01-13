4d261d19fd6118649822bd18bdbe0ce6b134319b
balarue <balarue023@gmail.com>
2026-01-12 21:56:52 -0600

    Added katas.py file for my first kata

---
+++ katas.py
@@ -0,0 +1,11 @@
+#String ends with? KATA
+#My solution - terrible
+#def solution(text, ending):
+ #   l = len(ending)
+  #  if ending == fw:
+        #return True
+    #else:
+       # return False
+# best soultion
+#def solution(string, ending):
+    #return string.endswith(ending)
