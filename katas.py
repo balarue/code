cd8e39b122c65a1d1f90410030d1aa0c564442e1
balarue <balarue023@gmail.com>
2026-01-13 18:49:49 -0600

    Completed another Kata

--- katas.py
+++ katas.py
@@ -9,3 +9,9 @@
 # best soultion
 #def solution(string, ending):
     #return string.endswith(ending)
+
+#jaden kata
+#best solution
+# def to_jaden_case(string):
+#     return " ".join(word.capitalize() for word in string.split())
+# answer due to help of AI
