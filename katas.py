fa8e5b762c618357ca6ef64d05185cd332522edc
balarue <balarue023@gmail.com>
2026-01-14 21:52:30 -0600

    Compelted another kata

--- katas.py
+++ katas.py
@@ -15,3 +15,30 @@
 # def to_jaden_case(string):
 #     return " ".join(word.capitalize() for word in string.split())
 # answer due to help of AI
+
+#Tribonacci Sequence
+# def tribonacci(signature, n):
+#     if n == 1:
+#         return [signature[0]]
+#     elif n == 0:
+#         return []
+#     elif n == 2:
+#         answer = signature[:2]
+#         return answer
+#     else:
+#         for i in range(n):
+#             if len(signature) < n:
+#                 l = signature[i] + signature[i+1] + signature[i+2]
+#                 signature.append(l)
+#             else:
+#                 return signature
+# tribonacci([1, 1, 1], 2)
+
+
+# def tribonacci(signature, n):
+#     for i in range(n):
+#         if len(signature) < n:
+#             l = signature[i] + signature[i+1] + signature[i+2]
+#             signature.append(l)
+#         else:
+#             return signature
